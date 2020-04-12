e87ae7754cca0f04f49670905e893a740af09194
jitendra754 <jitendra754@yahoo.co.in>
2020-04-12 16:18:32 +0530

    Playing with data type

---
+++ DataType.js
@@ -0,0 +1,8 @@
+var myName = "john";
+var score1 = 50;
+var score2 = 42.50;
+var sum = score1 + score2;
+console.log("Name: " + myName);
+console.log("First Score: " + score1);
+console.log("Second Score: " + score2);
+console.log("Sum Of Score: " + sum);
---
+++ DataType.ts
@@ -0,0 +1,9 @@
+var myName:string="john";
+var score1:number=50;
+var score2:number=42.50;
+var sum=score1 + score2;
+
+console.log("Name: " + myName);
+console.log("First Score: " + score1);
+console.log("Second Score: " + score2);
+console.log("Sum Of Score: " + sum);
