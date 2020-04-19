1a385722c4ad3a6f94bf40a3e934a45a6048ff44
jitendra754 <jitendra754@yahoo.co.in>
2020-04-19 16:25:56 +0530

    Interfaces;

---
+++ Interfaces.ts
@@ -0,0 +1,41 @@
+interface IPerson { 
+    firstName:string, 
+    lastName:string, 
+    sayHi: ()=>string 
+ } 
+ 
+ var customer:IPerson = { 
+    firstName:"Tom",
+    lastName:"Hanks", 
+    sayHi: ():string =>{return "Hi there"} 
+ } 
+ 
+ console.log("Customer Object ") 
+ console.log(customer.firstName) 
+ console.log(customer.lastName) 
+ console.log(customer.sayHi())  
+ 
+ var employee:IPerson = { 
+    firstName:"Jim",
+    lastName:"Blakes", 
+    sayHi: ():string =>{return "Hello!!!"} 
+ } 
+   
+ console.log("Employee  Object ") 
+ console.log(employee.firstName);
+ console.log(employee.lastName);
+
+ //Interface Inheritance
+ interface Person{
+     age:number
+ }
+ interface Musician extends Person{
+     instrument:string
+ }
+
+ var drummer=<Musician>{};
+ drummer.age=27;
+ drummer.instrument="Drums";
+
+ console.log(drummer.age);
+ console.log(drummer.instrument);
